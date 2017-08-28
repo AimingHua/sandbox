14c733ce45078ee207b2d10c28a5e95a607e5e53
Hua Aiming <ussthamcn@sina.com>
2017-08-28 14:36:25 +0800

    update

--- c++/binary.cpp
+++ c++/binary.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	char a = -58;
+	char a = -5;
 	std::bitset<8> x(a);
-	std::cout << x;
+	std::cout << x << endl;
 }
--- c++/memory_layout.c
+++ c++/memory_layout.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-short var_bss;
-short var_data=1;
+short var_bss1;
+short var_bss2;
+short var_data1=1;
 
 int main()
 {
@@ -14,14 +15,26 @@ int main()
     printf("*  code  segment  *\r\n");
     printf("*******************\r\n");
 
-    short var_stack;
+    static short var_data2=1;
+    short var_stack1;
+    short var_stack2;
     void *var_code = (void *)main;
-    short *var_heap = (short *)malloc(sizeof(short));
-    printf("stack segment: %p\r\n", &var_stack);
-    printf("heap  segment: %p\r\n", var_heap);
-    printf("bss   segment: %p\r\n", &var_bss);
-    printf("data  segment: %p\r\n", &var_data);
-    printf("code  segment: %p\r\n", var_code);
+    short *var_heap1 = (short *)malloc(sizeof(short));
+    short *var_heap2 = (short *)malloc(sizeof(short));
+    printf("stack segment: \r\n");
+    printf("&var_stack1: %p\r\n", &var_stack1);
+    printf("&var_stack2: %p\r\n", &var_stack2);
+    printf("heap segment: \r\n");
+    printf("&var_heap1: %p\r\n", var_heap1);
+    printf("&var_heap2: %p\r\n", var_heap2);
+    printf("bss segment: \r\n");
+    printf("&var_bss1: %p\r\n", &var_bss1);
+    printf("&var_bss2: %p\r\n", &var_bss2);
+    printf("data segment: \r\n");
+    printf("&var_data1: %p\r\n", &var_data1);
+    printf("&var_data2: %p\r\n", &var_data2);
+    printf("code segment: %p\r\n", var_code);
 
-    free(var_heap);
+    free(var_heap1);
+    free(var_heap2);
 }
