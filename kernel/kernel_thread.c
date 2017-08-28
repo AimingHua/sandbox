414253abff1cfcda32ef0e78062f9715e5e7ede5
Hua Aiming <ussthamcn@sina.com>
2017-08-28 17:44:18 +0800

    update

--- kernel/kernel_thread.c
+++ kernel/kernel_thread.c
@@ -18,3 +18,17 @@ static int kernel_init(void)
 {
     data = 20;
     printk("KERN_INFO", "---------------");
+    task = kthread_run(&thread_function, (void *)data, "aiming");
+    printk("KERN_INFO", kernel thread: %s\n, task->comm);
+    return 0;
+}
+
+static void kernel_exit(void)
+{
+    kthread_stop(task);
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+MODULE_AUTHOR("aiming");
+MODULE_LICENSE("GPL");
